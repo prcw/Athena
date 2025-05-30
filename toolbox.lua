24f0c2b0ef2f57b05d39ac98353cf0a95481402b
prcw <141184721+prcw@users.noreply.github.com>
2025-05-30 14:55:55 -0400

    toolbox.lua

--- toolbox.lua
+++ toolbox.lua
@@ -1,1 +1,14 @@
+local supported_places = {
+	[8204899140] = "Football Fusion 2",
+	[17541256525] = "Pro Servers",
+	[8206123457] = "New Fusion Practice"
+}
 
+local place = supported_places[game.PlaceId]
+
+if not place then return end 
+if not firetouchinterest then return end 
+
+local gs = function(sv)
+	return game:GetService(sv)
+end
