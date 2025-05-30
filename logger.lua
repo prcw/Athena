5b5223aabad5096ff6fc6da3af95c80987548ac1
prcw <141184721+prcw@users.noreply.github.com>
2025-05-30 14:52:53 -0400

    logger.lua

--- logger.lua
+++ logger.lua
@@ -1,1 +1,25 @@
+local logger = {}
 
+local prefix = "[athena]"
+
+local function format_message(level, ...)
+	local args = table.pack(...)
+	for i = 1, args.n do
+		args[i] = tostring(args[i])
+	end
+	return string.format("%s [%s] %s", prefix, level, table.concat(args, " "))
+end
+
+function logger.warn(...)
+	warn(format_message("warn", ...))
+end
+
+function logger.error(...)
+	error(format_message("error", ...), 2)
+end
+
+function logger.verbose(...)
+	print(format_message("verbose", ...))
+end
+
+return logger
